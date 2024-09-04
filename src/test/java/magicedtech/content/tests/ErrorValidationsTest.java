f9283eb8c416c58fc96bebc056ed6a7166f2d523
vibzz7 <vibhor.tripathi23@gmail.com>
2024-09-04 17:30:54 +0530

    changed the email address to trigger jenkins job

--- src/test/java/magicedtech/content/tests/ErrorValidationsTest.java
+++ src/test/java/magicedtech/content/tests/ErrorValidationsTest.java
@@ -15,7 +15,7 @@ public class ErrorValidationsTest extends BaseTest {
 	@Test(groups = {"ErrorHandling"}, retryAnalyzer=Retry.class)
 	public void LoginErrorValidation() throws IOException, InterruptedException {
 
-		landingPage.login("anshika@gmail.com", "Iamki000");
+		landingPage.login("anshikaa@gmail.com", "Iamki000");
 		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
 
 	}
